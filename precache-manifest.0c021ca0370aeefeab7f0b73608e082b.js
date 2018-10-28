14593fa2ad20fe7d8dad08128d1e76d0ad871042
Ashutosh Anand <ashutosh.bsc@gmail.com>
2018-10-28 18:15:49 +0530

    Updates

---
+++ precache-manifest.0c021ca0370aeefeab7f0b73608e082b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b468a6d73935f7cb57",
+    "url": "/kanbanboard/static/js/runtime~main.12b468a6.js"
+  },
+  {
+    "revision": "38baa626577554282e85",
+    "url": "/kanbanboard/static/js/main.38baa626.chunk.js"
+  },
+  {
+    "revision": "9f166c0be7240bf057df",
+    "url": "/kanbanboard/static/js/1.9f166c0b.chunk.js"
+  },
+  {
+    "revision": "38baa626577554282e85",
+    "url": "/kanbanboard/static/css/main.ff4c7f9d.chunk.css"
+  },
+  {
+    "revision": "e6dbff74b01c22eff0cd28cc41f00e39",
+    "url": "/kanbanboard/index.html"
+  }
+];
